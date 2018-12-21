82abff4768cc4b7536155f1f824a2797eb78ef48
paopao0218 <1643740208@qq.com>
2018-12-21 15:14:35 +0800

    初始化!

---
+++ src/main.js
@@ -0,0 +1,54 @@
+import {Calf} from './Calf';
+
+window.calf = new Calf({
+  el:'#app',
+  data:{
+    pageTitle:'mvvm 测试!',
+    user:{
+      username:{
+        firstName:"Adele",
+        lastName:"Adkins"
+      },
+      sex:1,
+    },
+    friends:[
+      {
+        username:{
+          firstName:"Scarlett",
+          lastName:"Johansson"
+        },
+        sex:1,
+      },
+      {
+        username:{
+          firstName:"Audrey",
+          lastName:"Hepburn"
+        },
+        sex:1,
+      },
+      {
+        username:{
+          firstName:"Kate",
+          lastName:"Winslet"
+        },
+        sex:1,
+      },
+      {
+        username:{
+          firstName:"Monica",
+          lastName:"Bellucci"
+        },
+        sex:1,
+      },
+      {
+        username:{
+          firstName:"Leonardo",
+          lastName:"DiCaprio"
+        },
+        sex:0,
+      },
+    ],
+  },
+  mounted(){
+  }
+});
