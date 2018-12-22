a1c6af62df2daca96f59bd22f2a5dd0caf8b6391
paopao0218 <1643740208@qq.com>
2018-12-22 16:54:17 +0800

    初始化!

--- src/Calf.js
+++ src/Calf.js
@@ -1,31 +1,40 @@
 import {Util} from './Util';
 
-
 export class Calf {
   constructor(params) {
-    this.cloneData = Util.deepClone(params.data);
-    this.dataSource = params.data;
+    this.dataSource = params.data;                // 可修改数据
+    this.cloneData = Util.deepClone(params.data); // 参考数据
     this.dataFrameCheck();
+    this.dataChangeHandles = {};
   }
   dataFrameCheck(){
     let that = this;
-    that.checkDataObj(that.dataSource);
+    that.checkDataObj();
     window.requestAnimationFrame(function(){
       that.dataFrameCheck();
     });
   }
-  checkDataObj(obj){
-    for(let [key, value] of Object.entries(obj)){
-      if(typeof value === 'object'){
-        if(Array.isArray(value)){
+  checkDataObj(){
+    let that = this;
+    for(let [key, value] of Object.entries(that.dataSource)){
+      if(typeof value === 'object' && value !== null) {
+        if (Array.isArray(value)) {
 
-        }else{
-
+        }else if(value instanceof Function){
+          //todo 函数待处理
+        } else {
         }
       }else{
-
+        if(value !== that.cloneData[key]){
+          let handles = that.dataChangeHandles[`${key}-changeHandles`];
+          if(handles && handles.length){
+            handles.forEach(handle => handle(value));
+          }
+          that.cloneData[key] = value;
+        }
       }
     }
   }
+
 
 }
